ae615ccdc60085fc0b4c629f68689c5fb239ab24
SupeChicken666 <supechicken666@gmail.com>
2024-03-28 01:21:18 +0800

    Register injection script on startup
    Fixes #1

--- service_worker.js
+++ service_worker.js
@@ -5,3 +5,26 @@ chrome.runtime.onInstalled.addListener(async i => {
       break;
   }
 });
+
+// register injection script on startup
+chrome.runtime.onStartup.addListener(async () => {
+  const localStorage = await chrome.storage.local.get(['backgroundURL', 'blurRadius', 'UIOpacity', 'chromeUI']);
+
+  if (!localStorage.backgroundURL) return;
+
+  chrome.scripting.registerContentScripts([{
+    allFrames: true,
+    persistAcrossSessions: false,
+    id:  'injection-script',
+    js:  ['js/inject.js'],
+    css: ['css/inject.css'],
+    matches: chromeURLs,
+    runAt: 'document_end'
+  }]).then(() => {
+    console.log('Injection script registered!');
+  }).catch(e => {
+    console.error(e);
+    console.error('Failed to register injection script.\n\nDoes the "Extensions on chrome:// URLs" flag enabled?');
+    throw new Error('Failed to register injection script.');
+  });
+});
